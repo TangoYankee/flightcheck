bd9cea016a4618bb018bf13a1305da424ceb9c4a
tangoyankee <miller.tim108@gmail.com>
2019-05-09 19:52:27 -0700

    move to flightAware module

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
 const flightAware = require('./flightaware.js');
 const slack = require('./slack.js');
-var config = require('./config');
+var config = require('./config.js');
+const package = require('./package.json')
 
 var express = require('express');
 const bodyParser = require('body-parser');
-
-var restclient = require('restler');
-
-// FlightAware
-const username = config.flight_aware.user;
-const apiKey = config.flight_aware.key;
-var fxml_url = config.flight_aware.url;
 
 var app = express();
 const PORT = config.port;
@@ -18,7 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
-    console.log(`slack-flightaware listening on port ${PORT}`)
+    console.log(`${package.name} listening on port ${PORT}`)
 });
 
 app.get('/', (req, res) => {
@@ -26,35 +20,15 @@ app.get('/', (req, res) => {
 });
 
 app.get('/oauth', (req, res) => {
-    slack.oauth(req, res);
+    slack.data.oauth(req, res);
 });
 
 app.post('/inflightinfo', (req, res)=>{
     var call_sign = req.body.text
-    restclient.get(`${fxml_url}InFlightInfo`, {
-        username: username,
-        password: apiKey,
-        query: {ident: call_sign}
-    }).on('success', (result, response) => {
-        var flight = result.InFlightInfoResult;
-        flight_string = `Call Sign: ${flight.ident}; long, lat: ${flight.longitude}, ${flight.latitude}; update type: ${flight.updateType}`;
-        res.send(flight_string);
-    });
+    flightAware.data.getInFlightInfo(res, call_sign);
 });
 
 app.post('/enroute', (req, res)=>{
     var airport = req.body.text
-    restclient.get(`${fxml_url}Enroute`, {
-        username: username,
-        password: apiKey,
-        query: {airport: airport, howMany: 15, order: 'estimatedarrivaltime', sort: 'DSC'}
-    }).on('success', (result, response) => {
-        var flights = result.EnrouteResult.enroute;
-        var schedule = "Early Send"
-        flights.forEach(flight=> {
-            console.log(`Aircraft Call Sign: ${flight.ident}, Depart Time: ${flight.actualdeparturetime}`);
-        });
-        res.send(`Call Sign: ${flights[0].ident}, Depart Time: ${flights[0].actualdeparturetime}`)
-    });
+    flightAware.data.getEnroute(res, airport);
 });
-
--- flightaware.js
+++ flightaware.js
@@ -7,32 +7,39 @@ const fxml_url = config.flight_aware.url
 
 var methods = {};
 
-methods.getEnroute = function(airport){
+methods.getEnroute = function(res, airport){
     restclient.get(`${fxml_url}Enroute`, {
         username: username,
         password: apiKey,
         query: {airport: airport, howMany: 15, order: 'estimatedarrivaltime', sort: 'DSC'}
     }).on('success', (result, response) => {
-        var flights = result.EnrouteResult.enroute;
-        // var schedule = [];
-        // entries.forEach((entry, index) => {
-        //     console.log(`Aircraft Call Sign: ${entry.ident}, Depart Time: ${entry.actualdeparturetime}`);
-        //     // schedule + `Aircraft Call Sign: ${entry.ident}, Depart Time: ${entry.actualdeparturetime}`;
-        //     // console.log(schedule)
-        // });
-        return flights        
+        var entries = result.EnrouteResult.enroute;
+        var schedule = "";
+        var count = 0;
+        entries.forEach((entry, index) => {
+            var flight = `Aircraft Call Sign: ${entry.ident}, Depart Time: ${entry.actualdeparturetime} \n` 
+            count++
+            // TODO: Create a well formatted message for enRoute flights
+            schedule + flight;
+            console.log(schedule);
+        });
+        console.log(schedule)
+        if (count == entries.length){
+            res.send(count.toString())
+        }        
     });
 }
 
-methods.getInFlightInfo = function (call_sign){
+methods.getInFlightInfo = function (res, call_sign){
     restclient.get(`${fxml_url}InFlightInfo`, {
         username: username,
         password: apiKey,
         query: {ident: call_sign}
     }).on('success', (result, response) => {
         var entry = result.InFlightInfoResult;
-        // console.log(`Call Sign: ${entry.ident}; long, lat: ${entry.longitude}, ${entry.latitude}; update type: ${entry.updateType}`);
-        return `Call Sign: ${entry.ident}; long, lat: ${entry.longitude}, ${entry.latitude}; update type: ${entry.updateType}`;   
+        var flight_string = `Call Sign: ${entry.ident}; long, lat: ${entry.longitude}, ${entry.latitude}; update type: ${entry.updateType}`
+        console.log(`module ${flight_string}`);
+        res.send(flight_string);
     });
 }
 
