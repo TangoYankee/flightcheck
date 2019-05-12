c35c0fffa2a9caf5f86a0be70cf641460c6a978a
tangoyankee <miller.tim108@gmail.com>
2019-05-11 22:59:37 -0700

    format block content, add api call for additional information

--- app.js
+++ app.js
@@ -23,31 +23,59 @@ app.get('/oauth', (req, res) => {
 
 app.post('/inflightinfo', (req, res)=>{
     var call_sign = req.body.text
-    flightAware.data.getInFlightInfo(res, call_sign);
-});
-
-app.post('/enroute', (req, res)=>{
-        var channel_id = req.body.channel_id
-        console.log(channel_id)
+    var channel_id = req.body.channel_id
+    flightAware.data.getInFlightInfo(res, call_sign, channel_id);
+    var flight_info = flightAware.data.getFlightInfo(call_sign);
+    var inflight_info = flightAware.data.getInFlightInfo(call_sign);
+    if (flight_info && inflight_info) {
         var message = {
-            "channel": `${channel_id}`,
-            "blocks": [
+        "channel": `${channel_id}`,
+        "blocks": [
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": `
+                        *Call Sign:* ${flight_info.ident}
+                        *Type Aircraft:* ${flight_info.aircrafttype}
+                        *Filed Departure Time:* ${flight_info.filed_departuretime}
+                        *Actual Depature Time:* ${flight_info.actualdeparturetime}
+                        *Estimated Arrival Time:* ${flight_info.estimatedarrivaltime}
+                        *Actual Arrival Time*: ${flight_info.actualarrivaltime}
+                        *Diverted:* ${flight_info.diverted}
+                        *Origin:* ${flight_info.originName}, ${flight_info.originCity} (${flight_info.origin})
+                        *Destination:* ${flight_info.destinationName}, ${flight_info.destinationCity} (${flight_info.destination})`
+                    }
+                },
                 {
                     "type": "image",
                     "title": {
                         "type": "plain_text",
-                        "text": "Example Image",
+                        "text": `${flight_info.ident} map preview`,
                         "emoji": true
                     },
-                    "image_url": "https://api.slack.com/img/blocks/bkb_template_images/goldengate.png",
-                    "alt_text": "Example Image"
+                    "image_url": `https://open.mapquestapi.com/staticmap/v5/map?locations=${inflight_info.latitude},${inflight_info.longitude}&size=@2x&zoom=8&key=${config.mapquest.maps_key}`,
+                    "alt_text": "map of aircraft"
+                }
+            ],
+        "attachments": [
+            {
+            "fallback": `View your flights at https://www.openstreetmap.org/?mlat=${inflight_info.latitude}&mlon=${inflight_info.longitude}&zoom=9#map=9/${inflight_info.latitude}/${inflight_info.longitude}`,
+            "actions": [
+                {
+                "type": "button",
+                "text": "View full map",
+                "url": `https://www.openstreetmap.org/?mlat=${inflight_info.latitude}&mlon=${inflight_info.longitude}&zoom=9#map=9/${inflight_info.latitude}/${inflight_info.longitude}`
                 }
             ]
-          }
-        res.json(message)
-    });
+            }
+        ]
+        }
+    res.json(message)
+    }
+});
 
-// app.post('/enroute', (req, res)=>{
-//     var airport = req.body.text
-//     flightAware.data.getEnroute(res, airport);
-// });
+app.post('/enroute', (req, res)=>{
+    var airport = req.body.text
+    flightAware.data.getEnroute(res, airport);
+});
--- flightaware.js
+++ flightaware.js
@@ -14,6 +14,7 @@ methods.getEnroute = function(res, airport){
         query: {airport: airport, howMany: 15, order: 'estimatedarrivaltime', sort: 'DSC'}
     }).on('success', (result) => {
         var flights = result.EnrouteResult.enroute;
+        // console.log(flights);
         var schedule = [];
         var count = 0;
         flights.forEach((flight, index) => {
@@ -26,15 +27,30 @@ methods.getEnroute = function(res, airport){
     });
 }
 
-methods.getInFlightInfo = function (res, call_sign){
+methods.getFlightInfo = function (call_sign){
+    restclient.get(`${fxml_url}FlightInfo`, {
+        username: username,
+        password: apiKey,
+        query: {ident: call_sign, howMany: 1}
+    }).on('success', (result) => {
+        var flights_info = result.FlightInfoResult;
+        var flight_info = flights_info.flights[0];
+        console.log(flight_info.ident);
+        return flights_info.flights[0]
+    });
+}
+
+methods.getInFlightInfo = function (res, call_sign, channel_id){
     restclient.get(`${fxml_url}InFlightInfo`, {
         username: username,
         password: apiKey,
         query: {ident: call_sign}
     }).on('success', (result) => {
-        var flight_info = result.InFlightInfoResult;
-        res.send(`position of ${call_sign}\nLatitude: ${flight_info.longitude}, Latitude: ${flight_info.latitude}
-        \nhttps://www.google.com/maps/search/${flight_info.latitude},${flight_info.longitude}/@${flight_info.latitude}059,-122.2704844,13z`);
+        // var inflight_info = result.InFlightInfoResult;
+        console.log(inflight_info);
+        return result.InFlightInfoResult;
+        // res.send(`position of ${call_sign}\nLatitude: ${inflight_info.longitude}, Latitude: ${inflight_info.latitude}
+        // \nhttps://www.google.com/maps/search/${inflight_info.latitude},${inflight_info.longitude}/@${inflight_info.latitude}059,-122.2704844,13z`);
     });
 }
 
