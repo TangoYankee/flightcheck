28b13f2f0455122cd0d0c4e84862e4b8eff2e85b
tangoyankee <miller.tim108@gmail.com>
2019-05-18 18:05:48 -0700

    check that values exist. handle missing values

--- config.ex.sh
+++ config.ex.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env_bash
 export SLACK_CLIENT_ID=""
 export SLACK_CLIENT_SECRET=""
-export SLACK_URL=""
 export MAPQUEST_MAPS_KEY=""
 export FLIGHTSTATS_KEY=""
 export FLIGHTSTATS_APP_ID=""
