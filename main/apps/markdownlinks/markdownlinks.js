55ff7a9fa2746cf48494db281386e40281616047
tangoyankee <miller.tim108@gmail.com>
2019-07-12 07:14:42 -0700

    rename functions, remove preview functionality

--- main/apps/markdownlinks/markdownlinks.js
+++ main/apps/markdownlinks/markdownlinks.js
@@ -3,25 +3,25 @@ var messages = require('./messages.js');
 var format = require('./format.js');
 var methods = {};
 
-methods.simpleLinks = (request_body, response) => {
-    var request_text = request_body.text;
-    var request_response_url = request_body.response_url;
-    var request_user_id = request_body.user_id;
-    if (request_text.trim() == "help") {
-        response.json(messages.data.help(`Welcome to Simple Links, <@${request_user_id}>!`))
-    } else if (request_text) {
+methods.markdown = (request_body, response) => {
+    var text = request_body.text;
+    var response_url = request_body.response_url;
+    var user_id = request_body.user_id;
+    if (text.trim() == "help") {
+        response.json(messages.data.help(`welcome to markdownlinks, <@${user_id}>!`))
+    } else if (text) {
         response.send()
-        format_body = format.data.formatLinks(request_text);
-        response_body_preview = messages.data.preview(format_body);
-        methods.sendPreview(response_body_preview, request_response_url);
+        markdown_format = format.data.formatLinks(text);
+        markdown_message = messages.data.markdown(markdown_format);
+        methods.sendMarkdown(markdown_message, response_url);
     } else {
         response.json(messages.data.help("please provide input"))
     }
 }
 
-methods.sendPreview = (response_body_preview, request_response_url) => {
+methods.sendMarkdown = (response_body_preview, response_url) => {
     request.post({
-        url: request_response_url,
+        url: response_url,
         body: response_body_preview,
         json: true,
     }, function (error, response, body) {
@@ -33,22 +33,20 @@ methods.sendPreview = (response_body_preview, request_response_url) => {
     })
 }
 
-methods.composeFinalMessage = (response_payload, response) => {
+methods.delete = (response_payload, response) => {
     response.send();
     var response_url = response_payload.response_url;
-    var message = response_payload.message.blocks[1].text.text;
     var value = response_payload.actions[0].value;
-    var user_id = response_payload.user.id;
     var response_message;
-    if (value == "publish"){
-        response_message = messages.data.publish(message, user_id);
-    } else if (value == "discard"){
-        response_message = messages.data.discard();
+    if (value == "delete"){
+        response_message = messages.data.delete();
+    } else {
+        response_message = messages.data.error();
     }
-    methods.sendFinalMessage(response_url, response_message)
+    methods.sendDelete(response_url, response_message)
 }
 
-methods.sendFinalMessage = (response_url, response_message) => {
+methods.sendDelete = (response_url, response_message) => {
     request.post({
         url: response_url,
         body: response_message,
@@ -57,7 +55,6 @@ methods.sendFinalMessage = (response_url, response_message) => {
         if (error) {
             console.log(`Error: ${error}`);
         } else {
-            // Insert a callback?
             console.log(`Body: ${body}, Response ${response}`);
         }
     })
--- main/apps/markdownlinks/messages.js
+++ main/apps/markdownlinks/messages.js
@@ -54,24 +54,10 @@ messages.help = (feedback) => {
     }
 }
 
-messages.preview = (markdown_message) => {
-    // First message is emphemeral and interactive, acting only as a preview
-    // Provides a preview of the link formatting with the message
-    // Provides two options: 
-    // Save message- triggers the final message, which is 'in_channel' and contains only formatted text
-    // Discard message- triggers ephemeral message that simply deletes the initial message
+messages.markdown = (markdown_message, user_id) => {
     return {
         "response_type": "in_channel",
         "blocks": [
-            {
-                "type": "context",
-                "elements": [
-                    {
-                        "type": "plain_text",
-                        "text": "message preview"
-                    }
-                ]
-            },
             {
                 "type": "section",
                 "text": {
@@ -83,61 +69,27 @@ messages.preview = (markdown_message) => {
                 "type": "divider"
             },
             {
-                "type": "actions",
-                "elements": [
-                    {
-                        "type": "button",
-                        "text": {
-                            "type": "plain_text",
-                            "text": "publish :heavy_check_mark:",
-                            "emoji": true
-                        },
-                        "value": "publish"
-                    },
-                    {
-                        "type": "button",
-                        "text": {
-                            "type": "plain_text",
-                            "text": "discard :negative_squared_cross_mark:",
-                            "emoji": true
-                        },
-                        "value": "discard"
-                    }
-                ]
-            }
-        ]
-    }
-}
-
-messages.publish = (markdown_message, user_id) => {
-    return {
-        "response_type": "in_channel",
-        "replace_original": "true",
-        "blocks": [
-            {
                 "type": "section",
                 "text": {
                     "type": "mrkdwn",
-                    "text": markdown_message
+                    "text": `shared by <@${user_id}>`
+                },
+                "accessory": {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "delete message :negative_squared_cross_mark:",
+                        "emoji": true
+                    },
+                    "value": "delete"
                 }
-            },
-            {
-                "type": "divider"
-            },
-            {
-                "type": "context",
-                "elements": [
-                    {
-                        "type": "mrkdwn",
-                        "text": `posted by <@${user_id}>`
-                    }
-                ]
             }
         ]
     }
 }
 
-messages.discard = () => {
+
+messages.delete = () => {
     // delete the initial message
     return {
         "response_type": "ephemeral",
@@ -148,6 +100,23 @@ messages.discard = () => {
                 "text": {
                     "type": "mrkdwn",
                     "text": "this message will self-destruct when your workspace refreshes"
+                }
+            }
+        ]
+    }
+}
+
+messages.error = () => {
+    // delete the initial message
+    return {
+        "response_type": "ephemeral",
+        "delete_original": "false",
+        "blocks": [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "command not recongized"
                 }
             }
         ]
