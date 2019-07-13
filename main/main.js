9199c618d2d924f5443a0c677beb16c022f00b7a
tangoyankee <miller.tim108@gmail.com>
2019-07-13 09:12:20 -0700

    remove markdown links folder

--- main/main.js
+++ main/main.js
@@ -1,6 +1,5 @@
 const flightstats = require('./apps/flightcheck/flightstats.js');
 const slack = require('./apps/slack/slack.js');
-const markdownlinks = require('./apps/markdownlinks/markdownlinks.js');
 var config = require('./config.js');
 
 var express = require('express');
@@ -16,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT);
 
 app.get('/', (request, response) => {
-    response.sendFile(path.join(__dirname + '/templates/markdownlinks.html'))
+    response.sendFile(path.join(__dirname + '/templates/index.html'))
 });
 
 app.get('/oauth', (request, response) => {
@@ -26,14 +25,3 @@ app.get('/oauth', (request, response) => {
 app.post('/flightcheck', (request, response) => {
     flightstats.data.controlInput(request.body.text, response);
 });
-
-app.post('/markdownlinks', (request, response) => {
-    markdownlinks.data.simpleLinks(request.body, response);
-});
-
-app.post('/confirm' , (request, response) => {
-    markdownlinks.data.composeFinalMessage(JSON.parse(request.body.payload), response);
-    // Function to direct response to message
-    var json_payload = JSON.parse(request.body.payload);
-    console.log(`payload: ${json_payload.response_url}`);
-})
